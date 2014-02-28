074cb6559a93db89651ca610adf24804b3e057cb
ZachAnders <Zacharia.Anders@gmail.com>
2014-02-27 23:48:47 -0700

    Renamed Powercode to DbSession.

--- Application.py
+++ Application.py
@@ -1,26 +1,31 @@
+#!/usr/bin/env python
 import json, os
 from flask import Flask, render_template, request, session, redirect, url_for, flash
 from AppAuth import has_valid_credentials, requires_session
-from Powercode import DbSession
+from DbSession import DbSession
 from PowercodeSchema import AccessPoint
 app = Flask(__name__)
 
+#Root, redirect to login
 @app.route("/")
 def hello():
 	return render_template("login.html")
 
+#Simple login page
 @app.route("/login", methods=['GET', 'POST'])
 def login():
 	if request.method == "POST":
 		user, pw = request.form["username"], request.form["password"]
 		if has_valid_credentials(user, pw):
 			#Set the username in the session. Apparently flask protects this from being tampered with
+			#via crypto sig
 			session["username"] = user
 			return redirect(url_for("allcall"))
 		return render_template("login.html", error="Incorrect Username or Password! Please double check them and try again.")
 	else:
 		return render_template("login.html")
 
+# Main page for selecting AlLCall APs
 @app.route("/allcall")
 @requires_session
 def allcall():
@@ -38,13 +43,15 @@ def allcall():
 	default_order = [(default_order[i], i%2) for i in range(len(default_order))]
 	return render_template("allcall.html", sites=sites, site_order=default_order, logged_in=True, debug=app.debug)
 
+#Select APs, determins appropriate customer list
 @app.route("/select_aps", methods=['POST'])
 @requires_session
 def select_aps():
 	aps = json.loads(request.form['selected_aps'])
-	aps = [int(ap.replace("ap_",'')) for ap in aps]
+	aps = [int(ap.replace("ap_", '')) for ap in aps]
 	return str(aps)
 
+#Logout.
 @app.route("/logout")
 def logout():
 	if "username" in session:
@@ -53,9 +60,14 @@ def logout():
 	return redirect(url_for("login"))
 
 if __name__ == "__main__":
-	app.secret_key = os.urandom(128)
 	app.debug = True
+	#This makes debugging a lot easier. Otherwise the current session gets invalidated every time flask restarts
 	if app.debug:
 		app.secret_key = 'U\xb3gP\x97\xd6\xd7\xe7\xce3<YO\xc4\x90\xfdp\xef4\xca\x13x\xd5\x83aq\x99rP/0Z'
+	else:
+		#Note to self: Figure out if this has any other repercussions.
+		#Pros: crypto key can't get leaked from source
+		#Cons: Restarting the web server invalidates old sessions. (Inconvenient?)
+		app.secret_key = os.urandom(128)
 	app.run()
 
