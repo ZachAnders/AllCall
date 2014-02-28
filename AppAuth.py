7484b8f65311ff00f6e872819e24375fdc14120e
ZachAnders <Zacharia.Anders@gmail.com>
2014-02-27 23:51:41 -0700

    Comments

--- AppAuth.py
+++ AppAuth.py
@@ -3,10 +3,13 @@ from functools import wraps
 
 def is_valid_user(some_user, timestamp):
 	"""Given a user, is this a valid user?"""
+	#TODO: Figure out powercode Auth and try to tie into it there
+	#TODO: Add timestamp check. Sessions should probably expire
 	if some_user == "admin":
 		return True
 
 def requires_session(some_route):
+	# Flask requires that decorators are implemented utilizing functools
 	@wraps(some_route)
 	def protected(*args, **kwargs):
 		if valid_session():
@@ -17,13 +20,14 @@ def requires_session(some_route):
 
 def valid_session():
 	"""Returns True only if there is currently a valid username in the session"""
-	#TODO: Add timestamp check
 	if "username" in session:
+		#TODO: Add timestamp
 		if is_valid_user(session["username"], 0):
 			return True
 	return False
 
 def has_valid_credentials(user, passw):
+	#TODO: Figure out powercode Auth and try to tie into it there
 	if user == "admin" and passw == "admin":
 		return True
 	return False
